a9d0a09e6fe9c76db789ee8332f4d22c9e12e51f
Chi Señires <chi.senires@gmail.com>
2015-06-25 21:58:13 +0800

    initial mobile layout

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<title>Wildcard | Official Blog of the Computer Society of the Ateneo</title>
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/lightslider.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+
+	<!-- Favicon -->
+	<link rel="icon" type="image/x-icon" href="img/wildcard-favicon.png">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<nav class="navbar navbar-default">
+			  <div class="container-fluid">
+			    <!-- Brand and toggle get grouped for better mobile display -->
+			    <div class="navbar-header">
+			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#wildcard-nav" aria-expanded="false">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar" aria-hidden="true"></span>
+			        <span class="icon-bar" aria-hidden="true"></span>
+			        <span class="icon-bar" aria-hidden="true"></span>
+			      </button>
+			      <a class="navbar-brand" href="#">
+			      	<!-- <img alt="Brand" src="img/wildcard-favicon.png"> -->
+			      	<img alt="Brand" src="img/wildcard-header.png">
+			      </a>
+			    </div><!-- /.navbar-header -->
+
+			    <!-- Collect the nav links, forms, and other content for toggling -->
+			    <div class="collapse navbar-collapse" id="wildcard-nav">
+			      <ul class="nav navbar-nav">
+			        <li class="dropdown">
+			        	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">CompSAt <span class="caret" aria-hidden="true"></span></a>
+				          <ul class="dropdown-menu">
+				            <li><a href="#">Select*</a></li>
+				            <li><a href="#">Events</a></li>
+				          </ul>
+			        </li>
+			        <li><a href="#">Information Technology</a></li>
+			        <li class="dropdown">
+			        	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Creative <span class="caret" aria-hidden="true"></span></a>
+			        	<ul class="dropdown-menu">
+			        		<li><a href="#">Movies</a></li>
+			        		<li><a href="#">Books</a></li>
+			        		<li><a href="#">Video Games</a></li>
+			        		<li><a href="#">I.T. Out</a></li>
+			        	</ul>
+			        </li>
+			      </ul>
+			    </div><!-- /.navbar-collapse -->
+			  </div><!-- /.container-fluid -->
+			</nav>
+		</div><!-- /.row -->
+
+		<div class="content">
+			<div class="row">
+				<div class="col-xs-12">
+					<ul id="featured">
+						<li data-thumb="img/thumbs/test-main-01.jpg">
+							<img src="img/test-main-01.jpg">
+							<div class="featured-content">
+								<h2 class="title">Article Title</h2>
+								<h5 class="author">by Author Name</h5>
+								<p>This is a short excerpt of the featured article. This should be a maximum of two lines/sentences only.</p>
+								<p class="button"><a class="btn btn-info btn-sm" role="button" href="#">Read More</a></p>
+							</div>
+						</li>
+						<li data-thumb="img/thumbs/test-main-02.jpg">
+							<img src="img/test-main-02.jpg">
+							<div class="featured-content">
+								<h2 class="title">Article Title</h2>
+								<h5 class="author">by Author Name</h5>
+								<p>This is a short excerpt of the featured article. This should be a maximum of two lines/sentences only.</p>
+								<p class="button"><a class="btn btn-info btn-sm" role="button" href="#">Read More</a></p>
+							</div>
+						</li>
+						<li data-thumb="img/thumbs/test-main-03.jpg">
+							<img src="img/test-main-03.jpg">
+							<div class="featured-content">
+								<h2 class="title">Article Title</h2>
+								<h5 class="author">by Author Name, Another Name</h5>
+								<p>This is a short excerpt of the featured article. This should be a maximum of two lines/sentences only.</p>
+								<p class="button"><a class="btn btn-info btn-sm" role="button" href="#">Read More</a></p>
+							</div>
+						</li>
+					</ul>
+				</div><!-- /.cols -->
+			</div><!-- /.row -->
+
+			<!-- start of Article Cards -->
+			<div class="row articles">
+				<div class="col-xs-12">
+					<div class="article-card">
+						<div class="article-header">
+							<img src="img/test-main-03.jpg">
+							<h2 class="title"><a href="#">Article Title</a></h2>
+						</div><!-- /.article-header -->
+						<div class="article-body">
+							<h5 class="author">By Author Name, Another Name</h5>
+							<p>
+								This is just a preview of what is in the article. It may contain the excerpt from the actual article.
+							</p>
+							<p>
+								It may also contain another paragraph, to give users a taste of what is in the article. I am just blabbering right now.
+							</p>
+						</div><!-- /.article-body -->
+						<div class="article-footer">
+							<ul class="article-buttons">
+								<li class="share">
+									<a href="#"><span class="glyphicon glyphicon-share-alt" aria-hidden="true"></span></a>
+								</li>
+								<li class="read-more">
+									<a href="#"><span style="font-weight: bold; font-size: 1.65em;">Read More</span></a>
+								</li>
+							</ul>
+						</div><!-- /.article-footer -->
+						<div class="clearfix"></div>
+					</div><!-- /.article-card -->
+
+					<div class="article-card">
+						<div class="article-header">
+							<img src="img/test-main-02.jpg">
+							<h2 class="title"><a href="#">Article Title</a></h2>
+						</div><!-- /.article-header -->
+						<div class="article-body">
+							<h5 class="author">By Author Name</h5>
+							<p>
+								This is just a preview of what is in the article. It may contain the excerpt from the actual article.
+							</p>
+							<p>
+								It may also contain another paragraph, to give users a taste of what is in the article. I am just blabbering right now.
+							</p>
+						</div><!-- /.article-body -->
+						<div class="article-footer">
+							<ul class="article-buttons">
+								<li class="share">
+									<a href="#"><span class="glyphicon glyphicon-share-alt" aria-hidden="true"></span></a>
+								</li>
+								<li class="read-more">
+									<a href="#"><span style="font-weight: bold; font-size: 1.65em;">Read More</span></a>
+								</li>
+							</ul>
+						</div><!-- /.article-footer -->
+						<div class="clearfix"></div>
+					</div><!-- /.article-card -->
+
+					<div class="article-card">
+						<div class="article-header">
+							<img src="img/test-main-01.jpg">
+							<h2 class="title"><a href="#">Article Title</a></h2>
+						</div><!-- /.article-header -->
+						<div class="article-body">
+							<h5 class="author">By Author Name</h5>
+							<p>
+								This is just a preview of what is in the article. It may contain the excerpt from the actual article.
+							</p>
+							<p>
+								It may also contain another paragraph, to give users a taste of what is in the article. I am just blabbering right now.
+							</p>
+						</div><!-- /.article-body -->
+						<div class="article-footer">
+							<ul class="article-buttons">
+								<li class="share">
+									<a href="#"><span class="glyphicon glyphicon-share-alt" aria-hidden="true"></span></a>
+								</li>
+								<li class="read-more">
+									<a href="#"><span style="font-weight: bold; font-size: 1.65em;">Read More</span></a>
+								</li>
+							</ul>
+						</div><!-- /.article-footer -->
+						<div class="clearfix"></div>
+					</div><!-- /.article-card -->
+
+				</div><!-- /.cols -->
+			</div><!-- /.row -->
+		</div><!-- /.content -->
+
+		<footer class="footer">
+			<a href="#"><img src="img/CompSAt_Gear.png" width="25" height="25"></a>
+			<p>&copy; 2015</p>
+		</footer>
+	</div><!-- /.container -->
+	<!-- jQuery -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/lightslider.min.js"></script>
+  <script src="js/config.js"></script>
+</body>
+</html>
---
+++ js/config.js
@@ -0,0 +1,13 @@
+$(function() {
+	$('#featured').lightSlider({
+		item: 1,
+		slideMargin: 0,
+		mode: 'slide',
+		loop: true,
+		auto: true,
+		pause: 100,
+		gallery: true,
+		thumbItem: 3,
+		currentPagerPosition: 'left'
+	});
+});
